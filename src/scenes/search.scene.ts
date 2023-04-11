620f247516aa794be33a0b68c6f4bb1f92a8c3c4
Jannis Hajda <jannis.hajda@protonmail.com>
2023-04-11 13:47:33 +0200

    Added search scene

--- src/scenes/search.scene.ts
+++ src/scenes/search.scene.ts
@@ -1,7 +1,49 @@
+import { Markup } from "telegraf";
 import { CommandCallbackContext, RegexCallbackContext } from "../types";
+
+const message = (page: number, query?: string) => {
+  if (query) {
+    return `Suche nach ${query} auf Seite ${page}`;
+  }
+
+  return `Suche alle Mensen auf Seite ${page}`;
+};
+
+const keyboard = (page: number, query?: string) => {
+  return Markup.inlineKeyboard([
+    [
+      {
+        text: "⬅️",
+        callback_data: `search:${page - 1}:${query || ""}`,
+      },
+      {
+        text: "➡️",
+        callback_data: `search:${page + 1}:${query || ""}`,
+      },
+    ],
+    [
+      {
+        text: "↩️ Zurück",
+        callback_data: "start",
+      },
+    ],
+  ]);
+};
 
 const cmd = (ctx: CommandCallbackContext) => {};
 
-const action = (ctx: RegexCallbackContext) => {};
+const action = (ctx: RegexCallbackContext) => {
+  const [_, pageString, query] = ctx.match;
+
+  const page = Number(pageString);
+
+  // check if page is out of range
+  if (page < 0) return ctx.answerCbQuery("Keine vorherigen Mensen gefunden!");
+
+  const msg = message(page, query);
+  const replyMarkup = keyboard(page, query);
+
+  ctx.editMessageText(msg, replyMarkup);
+};
 
 export { cmd, action };
